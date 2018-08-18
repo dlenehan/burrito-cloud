243280df5b6786b98775cad52abdadee7795b1f4
Deirdre Lenehan <dklenehan@eircom.net>
2018-08-18 19:49:55 +0100

    add noOpPasswordEncoder to get inMemoryAuthentication to work

---
+++ src/main/java/burritos/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package burritos.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+	auth.
+	inMemoryAuthentication()
+	.passwordEncoder(NoOpPasswordEncoder.getInstance())
+	.withUser("buzz")
+	.password("infinity")
+	.authorities("ROLE_USER")
+	.and()
+	.withUser("woody")
+	.password("bullseye")
+	.authorities("ROLE_USER");
+}
+	
+}
